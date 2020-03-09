74b30710bf8e4c8542830ab723d565e3d6fba0c6
fede-infa <infantino.fede@gmail.com>
2020-03-09 18:04:30 -0300

    Consumo API y generacion de lista
    Se consume API Pokemon y se genera una lista con botones de 'previous' y 'next'

--- src/js/pokedex.js
+++ src/js/pokedex.js
@@ -1,11 +1,71 @@
 /// <reference types="jquery" />
 
-const ALL_POKEMONS = {};
-let url = 'https://pokeapi.co/api/v2/pokemon';
-let nextUrl = null;
+// const ALL_POKEMONS = {};
+// let url = 'https://pokeapi.co/api/v2/pokemon';
+// let nextUrl = null;
+
+let nextUrl = '';
+let previousUrl = '';
+
+const nextButton = document.querySelector('#next');
+const previousButton = document.querySelector('#previous');
+const $ol = document.querySelector('.pokemon-list');
+
+const apiData = {
+    url: 'https://pokeapi.co/api/v2/',
+    type: 'pokemon',
+    id: ''
+}
+
+const {url, type, id} = apiData;
+let apiUrl = `${url}${type}${id}`;
+let pokemonInfo = {};
+
+async function requestApi(apiUrl){
+    fetch (apiUrl)
+    .then( response => response.json() )
+    .then( response => generateHtml(response));
+} 
 
 
 
+async function generateHtml (response) {
+
+    console.log(response);
+    nextUrl = response.next;
+    previousUrl = response.previous;
+    response.results.forEach( (pokemon, i) =>{
+
+        const newLi = document.createElement('li');
+        const liContent = document.createTextNode(pokemon.name);
+
+        console.log(`prueba ${i}`);
+
+        newLi.appendChild(liContent);
+        $ol.appendChild(newLi);
+    });
+} 
+
+
+document.querySelector('#previous').onclick = () =>{
+    const response = requestApi(previousUrl);
+    $("#pokelist").empty();
+    generateHtml(response);
+}
+
+document.querySelector('#next').onclick = () =>{
+    const response = requestApi(nextUrl);
+    $("#pokelist").empty();
+    generateHtml(response);
+}
+
+
+requestApi(apiUrl);
+
+
+
+
+/* 
 function getPokemonList(url){
     fetch(url)
         .then( response => response.json())
@@ -40,3 +100,4 @@ function addPokemonToList(pokemons){
 }
 
 getPokemonList();
+ */
